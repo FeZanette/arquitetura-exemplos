b4597bb0282830ba3bdc7c51f67fb5808de39f29
Fernanda Zanette <fernandazanette1975@gmail.com>
2024-02-20 17:30:06 -0300

    Arquitetura I Exemplos finalizada

--- src/controller/AccountController.ts
+++ src/controller/AccountController.ts
@@ -2,28 +2,33 @@ import { Request, Response } from "express"
 import { AccountDatabase } from "../database/AccountDatabase"
 import { Account } from "../models/Account"
 import { AccountDB } from "../types"
+import { AccountBusiness } from "../business/AccountBusiness"
 
 export class AccountController {
     public getAccounts = async (req: Request, res: Response) => {
         try {
-            const accountDatabase = new AccountDatabase()
-            const accountsDB: AccountDB[] = await accountDatabase.findAccounts()
-    
-            const accounts = accountsDB.map((accountDB) => new Account(
-                accountDB.id,
-                accountDB.balance,
-                accountDB.owner_id,
-                accountDB.created_at
-            ))
-    
-            res.status(200).send(accounts)
+
+            const accountBusiness = new AccountBusiness()
+            const output = await accountBusiness.getAccounts()
+
+            // const accountDatabase = new AccountDatabase()
+            // const accountsDB: AccountDB[] = await accountDatabase.findAccounts()
+
+            // const accounts = accountsDB.map((accountDB) => new Account(
+            //     accountDB.id,
+            //     accountDB.balance,
+            //     accountDB.owner_id,
+            //     accountDB.created_at
+            // ))
+
+            res.status(200).send(output)
         } catch (error) {
             console.log(error)
-    
+
             if (res.statusCode === 200) {
                 res.status(500)
             }
-    
+
             if (error instanceof Error) {
                 res.send(error.message)
             } else {
@@ -34,33 +39,40 @@ export class AccountController {
 
     public getAccountBalance = async (req: Request, res: Response) => {
         try {
-            const id = req.params.id
-    
-            const accountDatabase = new AccountDatabase()
-            const accountDB = await accountDatabase.findAccountById(id)
-    
-            if (!accountDB) {
-                res.status(404)
-                throw new Error("'id' não encontrado")
+            const input: any = {
+                id: req.params.id
             }
-    
-            const account = new Account(
-                accountDB.id,
-                accountDB.balance,
-                accountDB.owner_id,
-                accountDB.created_at
-            )
-    
-            const balance = account.getBalance()
-    
-            res.status(200).send({ balance })
+
+            const accountBusiness = new AccountBusiness()
+            const output = await accountBusiness.getAccountBalance(input)
+
+            // const id = req.params.id
+
+            // const accountDatabase = new AccountDatabase()
+            // const accountDB = await accountDatabase.findAccountById(id)
+
+            // if (!accountDB) {
+            //     res.status(404)
+            //     throw new Error("'id' não encontrado")
+            // }
+
+            // const account = new Account(
+            //     accountDB.id,
+            //     accountDB.balance,
+            //     accountDB.owner_id,
+            //     accountDB.created_at
+            // )
+
+            // const balance = account.getBalance()
+
+            res.status(200).send(output)
         } catch (error) {
             console.log(error)
-    
+
             if (res.statusCode === 200) {
                 res.status(500)
             }
-    
+
             if (error instanceof Error) {
                 res.send(error.message)
             } else {
@@ -71,50 +83,59 @@ export class AccountController {
 
     public createAccount = async (req: Request, res: Response) => {
         try {
-            const { id, ownerId } = req.body
-    
-            if (typeof id !== "string") {
-                res.status(400)
-                throw new Error("'id' deve ser string")
+
+            const input = {
+                id: req.body.id,
+                ownerId: req.body.ownerId
             }
-    
-            if (typeof ownerId !== "string") {
-                res.status(400)
-                throw new Error("'ownerId' deve ser string")
-            }
-    
-            const accountDatabase = new AccountDatabase()
-            const accountDBExists = await accountDatabase.findAccountById(id)
-    
-            if (accountDBExists) {
-                res.status(400)
-                throw new Error("'id' já existe")
-            }
-    
-            const newAccount = new Account(
-                id,
-                0,
-                ownerId,
-                new Date().toISOString()
-            )
-    
-            const newAccountDB: AccountDB = {
-                id: newAccount.getId(),
-                balance: newAccount.getBalance(),
-                owner_id: newAccount.getOwnerId(),
-                created_at: newAccount.getCreatedAt()
-            }
-    
-            await accountDatabase.insertAccount(newAccountDB)
-    
-            res.status(201).send(newAccount)
+
+            const accountBusiness = new AccountBusiness()
+            const output = await accountBusiness.createAccount(input)
+
+            // const { id, ownerId } = req.body
+
+            // if (typeof id !== "string") {
+            //     res.status(400)
+            //     throw new Error("'id' deve ser string")
+            // }
+
+            // if (typeof ownerId !== "string") {
+            //     res.status(400)
+            //     throw new Error("'ownerId' deve ser string")
+            // }
+
+            // const accountDatabase = new AccountDatabase()
+            // const accountDBExists = await accountDatabase.findAccountById(id)
+
+            // if (accountDBExists) {
+            //     res.status(400)
+            //     throw new Error("'id' já existe")
+            // }
+
+            // const newAccount = new Account(
+            //     id,
+            //     0,
+            //     ownerId,
+            //     new Date().toISOString()
+            // )
+
+            // const newAccountDB: AccountDB = {
+            //     id: newAccount.getId(),
+            //     balance: newAccount.getBalance(),
+            //     owner_id: newAccount.getOwnerId(),
+            //     created_at: newAccount.getCreatedAt()
+            // }
+
+            // await accountDatabase.insertAccount(newAccountDB)
+
+            res.status(201).send(output)
         } catch (error) {
             console.log(error)
-    
+
             if (res.statusCode === 200) {
                 res.status(500)
             }
-    
+
             if (error instanceof Error) {
                 res.send(error.message)
             } else {
@@ -125,42 +146,51 @@ export class AccountController {
 
     public editAccountBalance = async (req: Request, res: Response) => {
         try {
-            const id = req.params.id
-            const value = req.body.value
-    
-            if (typeof value !== "number") {
-                res.status(400)
-                throw new Error("'value' deve ser number")
+
+            const input = {
+                id: req.params.id,
+                value: req.body.value
             }
-    
-            const accountDatabase = new AccountDatabase()
-            const accountDB = await accountDatabase.findAccountById(id)
-    
-            if (!accountDB) {
-                res.status(404)
-                throw new Error("'id' não encontrado")
-            }
-    
-            const account = new Account(
-                accountDB.id,
-                accountDB.balance,
-                accountDB.owner_id,
-                accountDB.created_at
-            )
-    
-            const newBalance = account.getBalance() + value
-            account.setBalance(newBalance)
-    
-            await accountDatabase.updateBalanceById(id, newBalance)
-    
-            res.status(200).send(account)
+
+            const accountBusiness = new AccountBusiness()
+            const output = await accountBusiness.editAccountBalance(input)
+
+            // const id = req.params.id
+            // const value = req.body.value
+
+            // if (typeof value !== "number") {
+            //     res.status(400)
+            //     throw new Error("'value' deve ser number")
+            // }
+
+            // const accountDatabase = new AccountDatabase()
+            // const accountDB = await accountDatabase.findAccountById(id)
+
+            // if (!accountDB) {
+            //     res.status(404)
+            //     throw new Error("'id' não encontrado")
+            // }
+
+            // const account = new Account(
+            //     accountDB.id,
+            //     accountDB.balance,
+            //     accountDB.owner_id,
+            //     accountDB.created_at
+            // )
+
+            // const newBalance = account.getBalance() + value
+            // account.setBalance(newBalance)
+
+            // await accountDatabase.updateBalanceById(id, newBalance)
+
+            res.status(200).send(output)
         } catch (error) {
             console.log(error)
-    
+
             if (res.statusCode === 200) {
                 res.status(500)
             }
-    
+
             if (error instanceof Error) {
                 res.send(error.message)
             } else {
